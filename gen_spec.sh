9a88a89adefb3c033cf29cb121cb9cca96f9a77e
Tim Hughes <thughes@thegoldfish.org>
2014-05-05 22:35:15 +0100

    working on f20

--- gen_spec.sh
+++ gen_spec.sh
@@ -12,11 +12,10 @@ GEM_VERSION=''
 TMPDIR=$(mktemp -d)
 RETOUT=$(cd ${TMPDIR} && gem fetch ${GEM})
 RETVAL=$?
-echo ${RETOUT}
 if [ ${RETVAL} -eq 0 ]
 then 
     GEM_VERSION=$(echo ${RETOUT}|awk '{print $2}')
-    echo "DownloadedT: ${GEM_VERSION}"
+    echo "Downloaded: ${GEM_VERSION}"
 else 
     echo "Error: ${RETOUT}"
 fi
@@ -26,7 +25,9 @@ then
     gem2rpm -T > ${TMPL}
     echo "Generated new spec template: ${TMPL}"
 fi
-[ -f rubygem-${GEM}.spec.template ] 
-gem2rpm -t rubygem-${GEM}.spec.template ${TMPDIR}/${GEM_VERSION}.gem > rubygem-${GEM}.spec.test
+if [ -f rubygem-${GEM}.spec.template ] 
+then
+    gem2rpm -t rubygem-${GEM}.spec.template ${TMPDIR}/${GEM_VERSION}.gem > rubygem-${GEM}.spec
+fi
 [ -d ${TMPDIR} ] && rm -rf ${TMPDIR}
 
